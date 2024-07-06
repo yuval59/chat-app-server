d9dfc9afb4d21d9ab3afca9d23bd96791a8bd1eb
Yuval Maron <yuvalmaron222@gmail.com>
2024-07-06 18:45:26 +0300

    Added default param on channel model

--- src/db/models/channel.ts
+++ src/db/models/channel.ts
@@ -1,6 +1,7 @@
-import { mysqlTable, varchar } from 'drizzle-orm/mysql-core'
+import { boolean, mysqlTable, varchar } from 'drizzle-orm/mysql-core'
 
 export const ChannelModel = mysqlTable('channels', {
   id: varchar('id', { length: 36 }).primaryKey(),
   name: varchar('name', { length: 256 }).notNull(),
+  default: boolean('isDefault').default(false),
 })
